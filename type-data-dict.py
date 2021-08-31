325c8aa9affa6fb8d077ad6da9179226f39d4b32
mrazak <muhamadrozak9@gmail.com>
2021-08-31 16:13:12 +0700

    update dis condition

--- type-data-dict.py
+++ type-data-dict.py
@@ -13,7 +13,7 @@ data_dari_server_gojek = {
     'tanggal': '2021-08-31',
     'driver_list': [
         {'nama': 'Muhammad', 'jarak': 10},
-        {'nama': 'Mufassir', 'jarak': 30},
+        {'nama': 'Mufassir', 'jarak': 25},
         {'nama': 'Quran', 'jarak': 50},
     ]
 }
@@ -24,3 +24,9 @@ print(f"Driver #1 {data_dari_server_gojek['driver_list'][1]}")
 
 print('-'*10)
 print(f"Jarak Driver Terdekat {data_dari_server_gojek['driver_list'][1]['jarak']} meter")
+
+for a in data_dari_server_gojek['driver_list']:
+    if (a['jarak'] < 30):
+        print(f"Driver {a['nama']} berjarak {a['jarak']} Meter")
+
+
